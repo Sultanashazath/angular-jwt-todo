5febe53f8509f3467784c538385118c83e5c7993
sultana-mnw <sultana.s@mnw.co.in>
2019-03-23 09:02:23 +0530

    refernece

---
+++ client/src/app/todo.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Todo } from './todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  constructor(private http: HttpClient) { }
+
+  getTodos() {
+    console.log( Todo)
+    return this.http.get<Todo[]>('/api/todos');
+  }
+
+  getTodo(id: number) {
+    return this.http.get<Todo>(`/api/todos/${id}`);
+  }
+}
